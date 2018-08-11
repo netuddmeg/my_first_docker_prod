3a1bf8848d50b4f5da9112cdef12d6f7364d44d7
JohnDoe <you@example.com>
2018-08-11 22:00:24 +0200

    modified: startup script many times

--- start.sh
+++ start.sh
@@ -2,23 +2,38 @@
 
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 DOCKERURL="https://github.com/docker/machine/releases/download/v0.15.0"
-DOCKERMACHINE="docker-sandbox"
 
 export DEBIAN_FRONTEND=noninteractive;
 
 sudo -- sh -c 'apt-get install curl -y';
 
-DOWNLOAD=$(curl -L - $DOCKERURL/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine);
+DOWNLOAD=$(curl -L $DOCKERURL/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine);
 
 if [ $? != 0 ] ; then
-    echo "There was a problem downloading the script"
-    exit 1
+	echo "There was a problem downloading the script! Check your internet connection!"
+	exit 1;
 else
-    bash -c "$DOWNLOAD";
-    echo -n "Please, enter your token here [ENTER]: ";
-    read token;
-    DOTOKEN=$token;
+	bash -c "$DOWNLOAD";
 
-    docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN $DOCKERMACHINE;
+        clear;
+        echo -n "Please, enter machine name here [ENTER]: ";
+        read machine;
+        DOCKERMACHINE=$machine;
+	VALID=`docker-machine ls|cut -f1 -d " "|tail -1`;
 
-fi
+	if [ $DOCKERMACHINE = $VALID ] ; then
+		echo "INVALID machine name, its already exist! Choose another name!"
+                exit 1;
+        else
+
+		clear;
+        	echo -n "Please, enter your token here [ENTER]: ";
+	        read token;
+	        DOTOKEN=$token;
+		clear;
+		docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN $DOCKERMACHINE;
+                rm /tmp/docker-machine;
+	fi;
+
+
+fi;
