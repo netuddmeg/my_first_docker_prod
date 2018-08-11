e8f3afd692d001e46cbb390531b829a7a78d4559
Your Name <you@example.com>
2018-08-11 18:49:37 +0200

    modified: startup script

--- start.sh
+++ start.sh
@@ -10,4 +10,4 @@ curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && \
 sudo install /tmp/docker-machine /usr/local/bin/docker-machine;
 
 
-docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN docker-sandbox
+#docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN docker-sandbox
