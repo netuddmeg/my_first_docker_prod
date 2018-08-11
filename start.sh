dbe3b6757bf0749148ad7feec84b3fc7ef85826e
Your Name <you@example.com>
2018-08-11 20:14:35 +0200

    modified: startup script

--- start.sh
+++ start.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-export DEBIAN_FRONTEND=noninteractive;
 DOCKERURL="https://github.com/docker/machine/releases/download/v0.15.0"
 DOCKERMACHINE="docker-sandbox"
 
+export DEBIAN_FRONTEND=noninteractive;
 
-sudo -- sh -c 'apt-get update -y && \
-               apt-get install curl -y';
+sudo -- sh -c 'apt-get install curl -y';
 
 curl -L -C - $DOCKERURL/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && \
 sudo install /tmp/docker-machine /usr/local/bin/docker-machine;
 
 
-echo -n "Please, enter your token here [ENTER]:";
+echo -n "Please, enter your token here [ENTER]: ";
 read token;
 
 docker-machine create --driver digitalocean --digitalocean-access-token $token $DOCKERMACHINE;
