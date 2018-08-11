40b573c18484ed62d48f4061c4038ac209e6b1bc
JohnDoe <you@example.com>
2018-08-11 23:07:47 +0200

    modified: startup script many times

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 DOCKERURL="https://github.com/docker/machine/releases/download/v0.15.0"
+REPO="https://github.com/netuddmeg/my_first_docker_prod.git"
+PROJECTNAME="my_first_docker_project"
 
 export DEBIAN_FRONTEND=noninteractive;
 
@@ -31,9 +33,27 @@ else
 	        read token;
 	        DOTOKEN=$token;
 		clear;
-		docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN $DOCKERMACHINE;
+		docker-machine create --driver digitalocean \
+			--digitalocean-image ubuntu-16-04-5-x64 \
+		       	--digitalocean-access-token $DOTOKEN \
+			$DOCKERMACHINE;
                 rm /tmp/docker-machine;
 	fi;
+
+	IPADDR=`docker-machine ip docker-sandbox`;
+	if [ ! -z $IPADDR ]; then
+
+		docker-machine ssh $DOCKERMACHINE "export DEBIAN_FRONTEND=noninteractive && sudo apt-get install git curl -y";
+		docker-machine ssh $DOCKERMACHINE "git clone $REPO && cd my_first_docker_prod";
+		docker-machine ssh $DOCKERMACHINE "docker build -t $PROJECTNAME my_first_docker_prod/";
+		docker-machine ssh $DOCKERMACHINE "docker run --rm -p 80:80 -d -t $PROJECTNAME";
+
+	else
+		echo "Cannot find IP address!";
+		exit 1;
+	fi;
+
+
 
 
 fi;
