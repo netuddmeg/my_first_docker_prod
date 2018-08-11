d996c036510082de6af53d0bf139a7b10f70b8c0
Your Name <you@example.com>
2018-08-11 19:14:12 +0200

    modified: startup script

--- start.sh
+++ start.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-set -x
-
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export DEBIAN_FRONTEND=noninteractive;
+DOCKERURL="https://github.com/docker/machine/releases/download/v0.15.0"
+
 
 sudo -- sh -c 'apt-get update -y && apt-get upgrade -y && apt-get install curl -y';
 
-DOCKERURL=https://github.com/docker/machine/releases/download/v0.15.0 && \
 curl -L $DOCKERURL/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && \
 sudo install /tmp/docker-machine /usr/local/bin/docker-machine;
 
