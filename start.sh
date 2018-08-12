b877b218769ae38e2e35bafee9e9c3bfba4fcde5
JohnDoe <you@example.com>
2018-08-12 22:37:35 +0200

    modified with eval

--- start.sh
+++ start.sh
@@ -10,14 +10,12 @@ export DEBIAN_FRONTEND=noninteractive;
 
 sudo -- sh -c 'apt-get install curl -y';
 
-DOWNLOAD=$(sudo curl -L $DOCKERURL/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine);
+curl -L $DOCKERURL/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine);
 
 if [ $? != 0 ] ; then
 	echo "There was a problem downloading the script! Check your internet connection!"
 	exit 1;
 else
-	bash -c "$DOWNLOAD";
-
         clear;
         echo -n "Please, enter machine name here [ENTER]: ";
         read machine;
