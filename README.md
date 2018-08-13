78e8e27c53c6f310431e80d5e22bb7ceb4d71b44
Jozsef Valkai <jozsef.valkai@comp280.ssc.luxoffice.duodecadits.com>
2018-08-13 13:02:26 +0200

    modified README and start.sh

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Hi'
+Goal
+Deploy a webserver on the port 80 with docker and display the "Hello world" message.
 
-This is my first Docker repo.
+Install & run  from the scratch:
+bash <(curl -s https://raw.githubusercontent.com/netuddmeg/my_first_docker_prod/master/start.sh)
+
+
--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-DOCKERURL="https://github.com/docker/machine/releases/download/v0.15.0"
-REPODIR="my_first_docker_prod"
-REPO="https://github.com/netuddmeg/my_first_docker_prod.git"
-PROJECTNAME="my_first_docker_project"
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin";
+DOCKERURL="https://github.com/docker/machine/releases/download/v0.15.0";
+REPODIR="my_first_docker_prod";
+REPO="https://github.com/netuddmeg/my_first_docker_prod.git";
+PROJECTNAME="my_first_docker_project";
+PORTS="80";
 
 export DEBIAN_FRONTEND=noninteractive;
 
@@ -11,7 +12,7 @@ sudo -- sh -c 'apt-get install curl -y';
 
 curl -L $DOCKERURL/docker-machine-$(uname -s)-$(uname -m) > /tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine;
 
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
 	echo "There was a problem downloading the script! Check your internet connection!"
 	exit 1;
 else
@@ -21,7 +22,7 @@ else
         DOCKERMACHINE=$machine;
 	VALID=`docker-machine ls|cut -f1 -d " "|tail -1`;
 
-	if [ $DOCKERMACHINE = $VALID ]; then
+	if [ $DOCKERMACHINE = $VALID ] ; then
 		echo "INVALID machine name, its already exist! Choose another name!";
                 exit 1;
         else
@@ -42,7 +43,10 @@ else
 		docker-machine ssh $DOCKERMACHINE "git clone $REPO && cd $REPODIR";
                 eval $(docker-machine env $DOCKERMACHINE);
 		docker-machine ssh $DOCKERMACHINE "docker build -t $PROJECTNAME $REPODIR";
-		docker-machine ssh $DOCKERMACHINE "docker run --rm -p 80:80 -d -t $PROJECTNAME";
+		docker-machine ssh $DOCKERMACHINE "docker run --rm -p 80:$PORTS -d -t $PROJECTNAME";
+		echo "---------------------------------------------";
+		echo "Check the result at: ---> http://$IPADDR:$PORTS <---";
+		echo "---------------------------------------------";
 
 	else
 		echo "Cannot find IP address!";
