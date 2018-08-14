7668e2c45dbb29d6666482879d4c2008aa2962b2
Jozsef Valkai <jozsef.valkai@comp280.ssc.luxoffice.duodecadits.com>
2018-08-14 11:19:58 +0200

    modified: Dockerfiles

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
-// index.js
-const express = require('express');
-const app = express();
-app.get('/', (req, res) =>  {      
-   console.log('I just received a GET request on port 3000!');
-   res.send('Hello World!');
-});
-app.listen(3000, () => console.log('I just connected on port 3000!'));
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) =>  { 
+    
+    console.log('I just received a GET request on port 3000!')
+    res.send('Hello World!');
+})
+
+app.listen(3000, () => console.log('I just connected on port 3000!'))
