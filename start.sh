4e587488a813803f86b5ab573772277f8c6e7d45
Jozsef Valkai <jozsef.valkai@comp280.ssc.luxoffice.duodecadits.com>
2018-08-15 16:16:40 +0200

    modified start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,12 @@ DOCKERMACHINE="docker-sandbox-swarm";
 REPO="https://github.com/netuddmeg/my_first_docker_prod.git";
 PROJECTNAME="my_first_docker_project";
 PORTS="80";
-DM=~/docker-machine;
+DPATH=/usr/local/bin/;
 
 export DEBIAN_FRONTEND=noninteractive && sudo apt install curl -y;
 
 #if [ ! -f $DM  ] ; then
-#        curl -L $DMURL/docker-machine-$(uname -s)-$(uname -m) > ~/docker-machine && chmod +x $DM;
+#        curl -L $DMURL/docker-machine-$(uname -s)-$(uname -m) > $DPATH/docker-machine && chmod +x $DPATH/docker-machine;
 #fi;
 
 #        	echo -n "Please, enter your token here [ENTER]: ";
@@ -20,13 +20,9 @@ export DEBIAN_FRONTEND=noninteractive && sudo apt install curl -y;
 #		~/docker-machine create --driver digitalocean --digitalocean-access-token $DOTOKEN $DOCKERMACHINE;
 
 		~/docker-machine ssh $DOCKERMACHINE "export DEBIAN_FRONTEND=noninteractive && sudo apt-get install git -y";
-		~/docker-machine ssh $DOCKERMACHINE "curl -L $DMURL/docker-machine-$(uname -s)-$(uname -m) > ~/docker-machine && chmod +x ~/docker-machine";
+		~/docker-machine ssh $DOCKERMACHINE "curl -L $DMURL/docker-machine-$(uname -s)-$(uname -m) > $DPATH/docker-machine && chmod +x $DPATH/docker-machine";
 		~/docker-machine ssh $DOCKERMACHINE "git clone $REPO";
 		eval $(~/docker-machine env $DOCKERMACHINE);
-		~/docker-machine ssh $DOCKERMACHINE "curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) > ~/docker-compose;chmod +x ~/docker-compose";
-		~/docker-machine ssh $DOCKERMACHINE "cd $REPODIR; ~/docker-compose up --build";
-		echo "---------------------------------------------";
-		IPADDR=`$DM ip $DOCKERMACHINE`;
-		echo "Check the result at: ---> http://$IPADDR:$PORTS <---";
-		echo "---------------------------------------------";
+		~/docker-machine ssh $DOCKERMACHINE "curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) > $DPATH/docker-compose;chmod +x $DPATH/docker-compose";
+		~/docker-machine ssh $DOCKERMACHINE "cd $REPODIR; docker-compose up --build";
 
